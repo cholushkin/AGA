2bae5c250547ad28452e944fe947da2e9316b88d
max <cholushkin@gmail.com>
2023-06-14 22:45:56 +0300

    feature: add ScreenTransition class

---
+++ Unity/AGA/Assets/Game/UI/ScreenTransition/AnimationEventCallback.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationEventCallback : MonoBehaviour
+{
+    public ScreenTransition ScreenTransitionEffect;
+
+    public void OnAnimationFinished()
+    {
+        ScreenTransitionEffect.OnAnimationFinishCallBack(gameObject.name);
+    }
+}
---
+++ Unity/AGA/Assets/Game/UI/ScreenTransition/ScreenTransition.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Alg;
+using Assets.Plugins.Alg;
+using GameLib.Log;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+// Slightly inspired by : https://youtu.be/CE9VOZivb3I
+
+public class ScreenTransition : Singleton<ScreenTransition>
+{
+    public string LastEffectPlayed { get; private set; }
+    private List<Transform> _effects;
+    private GameObject _currentEffect;
+    private Action _currentCallback;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        _effects = transform.Children().ToList();
+
+        // hide all if user forgot to hide it
+        foreach (Transform effect in transform)
+            Assert.IsFalse(effect.gameObject.activeSelf);
+    }
+
+    public void PlayEffect(string effectName, Action endCallback)
+    {
+        if (_currentEffect != null)
+        {
+            _currentEffect.SetActive(false);
+            _currentEffect = null;
+        }
+        Assert.IsNull(_currentEffect);
+        var effect = FindEffect(effectName);
+        LastEffectPlayed = effectName;
+        effect.SetActive(true);
+        _currentEffect = effect;
+        _currentCallback = endCallback;
+    }
+
+    public void OnAnimationFinishCallBack(string effectName)
+    {               
+        LogChecker.Print(LogChecker.Level.Verbose, $"{effectName} finished");
+        Assert.IsNotNull(_currentEffect);
+        if (_currentEffect.GetComponent<TransitionEffect>().DisableOnFinish)
+        {
+            _currentEffect.SetActive(false);
+            _currentEffect = null;
+        }
+
+        _currentCallback?.Invoke();
+        _currentCallback = null;
+    }
+
+    private GameObject FindEffect(string effectName)
+    {
+        var effect = _effects.FirstOrDefault(e => e.name == effectName);
+        return effect.gameObject;
+    }
+
+}
