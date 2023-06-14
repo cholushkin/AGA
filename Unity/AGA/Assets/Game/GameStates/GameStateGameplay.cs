cd0aa712661f16ca4ddec53f0f923f9f915b8f24
max <cholushkin@gmail.com>
2023-06-14 22:25:15 +0300

    feature: add transitions between states

--- Unity/AGA/Assets/Game/GameStates/GameStateGameplay.cs
+++ Unity/AGA/Assets/Game/GameStates/GameStateGameplay.cs
@@ -1,19 +1,32 @@
 using GameLib.Log;
+using UnityEngine;
 
 public class GameStateGameplay : AppStateManager.AppState<GameStateGameplay>
 {
     public override void AppStateInitialization()
     {
         LogChecker.Print("AppStateInitialization");
+        gameObject.SetActive(false);
     }
 
     public override void AppStateEnter()
     {
         LogChecker.Print("AppStateEnter");
+        gameObject.SetActive(true);
     }
 
     public override void AppStateLeave()
     {
         LogChecker.Print("AppStateLeave");
+        gameObject.SetActive(false);
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            print("Debug finish game");
+            AppStateManager.Instance.Start<GameStateLevelSelect>();
+        }
     }
 }
--- Unity/AGA/Assets/Game/GameStates/GameStateLevelSelect.cs
+++ Unity/AGA/Assets/Game/GameStates/GameStateLevelSelect.cs
@@ -1,19 +1,32 @@
 using GameLib.Log;
+using UnityEngine;
 
 public class GameStateLevelSelect : AppStateManager.AppState<GameStateLevelSelect>
 {
     public override void AppStateInitialization()
     {
         LogChecker.Print("AppStateInitialization");
+        gameObject.SetActive(false);
     }
 
     public override void AppStateEnter()
     {
         LogChecker.Print("AppStateEnter");
+        gameObject.SetActive(true);
     }
 
     public override void AppStateLeave()
     {
         LogChecker.Print("AppStateLeave");
+        gameObject.SetActive(false);
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            print("Debug start game");
+            AppStateManager.Instance.Start<GameStateGameplay>();
+        }
     }
 }
