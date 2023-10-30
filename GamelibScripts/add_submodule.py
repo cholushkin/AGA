0ef2daf0bb898cb83df17baa1e58984632cbc44c
max <cholushkin@gmail.com>
2023-10-30 21:43:59 +0200

    chore: add uconsole submodule add: add_submodule script

---
+++ GamelibScripts/add_submodule.py
@@ -0,0 +1,29 @@
+import subprocess
+import sys
+
+def add_git_submodule(repo_url, submodule_path):
+    try:
+        subprocess.run(['git', 'submodule', 'add', repo_url, submodule_path], check=True)
+        print(f"Submodule added successfully: {submodule_path}")
+        
+        # Update the submodule after adding
+        update_submodule(submodule_path)
+    except subprocess.CalledProcessError as e:
+        print(f"Failed to add submodule: {e}")
+
+def update_submodule(submodule_path):
+    try:
+        subprocess.run(['git', 'submodule', 'update', '--init', submodule_path], check=True)
+        print(f"Submodule updated successfully: {submodule_path}")
+    except subprocess.CalledProcessError as e:
+        print(f"Failed to update submodule: {e}")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python add_submodule.py <repository_url> <submodule_path>")
+        sys.exit(1)
+
+    repository_url = sys.argv[1]
+    submodule_path = sys.argv[2]
+
+    add_git_submodule(repository_url, submodule_path)
---
+++ add_uconsole_submodule.cmd
@@ -0,0 +1,1 @@
+GamelibScripts\add_submodule.py https://github.com/cholushkin/uconsole.git Unity/AGA/Assets/Libs/uconsole
