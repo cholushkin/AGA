bfb976c0cfa5f8de6ac18d302230fd6d9a110317
max <cholushkin@gmail.com>
2024-02-10 18:32:42 +0200

    feature(CastleGenerator): Use1x1OnlyAsFiller

--- ArtSources/ExportCastleChunks.cmd
+++ ArtSources/ExportCastleChunks.cmd
@@ -1,7 +1,7 @@
 cd ExportScripts
 call config.cmd
-call export_fbx_object.cmd ../CastleChunks/CastleChunks.blend P1x1 CastleGenerator/T1.Omino/FBX/CastleChunks/P1x1.fbx
-call export_fbx_object.cmd ../CastleChunks/CastleChunks.blend P2x2.005 CastleGenerator/T1.Omino/FBX/CastleChunks/P2x2.005.fbx
-rem call export_fbx_object.cmd ../People.blend 03_person FBX/People/03_person.fbx
-rem call export_fbx_object.cmd ../People.blend 04_person FBX/People/04_person.fbx
-rem call export_fbx_object.cmd ../People.blend 08_person FBX/People/08_person.fbx
+rem call export_fbx_object.cmd ../CastleChunks/CastleChunks.blend P1x1 CastleGenerator/T1.Omino/FBX/CastleChunks/P1x1.fbx
+rem call export_fbx_object.cmd ../CastleChunks/CastleChunks.blend P2x2.005 CastleGenerator/T1.Omino/FBX/CastleChunks/P2x2.005.fbx
+rem call export_fbx_object.cmd ../CastleChunks/CastleChunks.blend P2x2.003 CastleGenerator/T1.Omino/FBX/CastleChunks/P2x2.003.fbx
+rem call export_fbx_object.cmd ../CastleChunks/CastleChunks.blend P1x2 CastleGenerator/T1.Omino/FBX/CastleChunks/P1x2.fbx
+call export_fbx_object.cmd ../CastleChunks/CastleChunks.blend P2x1 CastleGenerator/T1.Omino/FBX/CastleChunks/P2x1.fbx
--- Unity/AGA/Assets/Game/CastleGenerator/T1.Omino/CastlePolyominoGenerator.cs
+++ Unity/AGA/Assets/Game/CastleGenerator/T1.Omino/CastlePolyominoGenerator.cs
@@ -13,6 +13,9 @@ namespace CastleGenerator.Tier1
 {
     public class CastlePolyominoGenerator : MonoBehaviour
     {
+        public bool AdaptiveProbability;
+        public bool Use1x1OnlyAsFiller;
+        
         private byte[,] _data;
         private int _width;
         private int _height;
@@ -24,18 +27,24 @@ namespace CastleGenerator.Tier1
         private int[] _pieceTypeSpawnedCounter; // number of each type of the piece being spawned 
         private List<Vector2Int> _availableCells; // Set of all empty cells available for piece spawning
         private float[] _normProbs; // target percent of each piece (normalized). Example for 3 items: 0.25 0.25 0.5
-
-        private float[]
-            _normProbsCurrent; // target percent of each piece (normalized) modified to increase a chance to reach _normProbs. Get updated each spawn
+        private float[] _normProbsCurrent; // target percent of each piece (normalized) modified to increase a chance to reach _normProbs. Get updated each spawn
 
         private IPseudoRandomNumberGenerator _rnd;
         private List<Vector2Int> _perimeter;
+        private int _castleCellsCount;
 
         public void Init(CellGeneratorController cellGenerator, PolyominoProvider polyominoProvider,
             IPseudoRandomNumberGenerator rnd, LogChecker log)
         {
             _rnd = rnd;
             _polyominoProvider = polyominoProvider;
+
+            if (Use1x1OnlyAsFiller)
+            {
+                var p = _polyominoProvider.Polyominos.FirstOrDefault(p => p.Shape == "#");
+                if (p != null)
+                    p.Probability = 0f;
+            }
             _data = cellGenerator.Data;
             _width = _data.GetLength(0);
             _height = _data.GetLength(1);
@@ -47,14 +56,15 @@ namespace CastleGenerator.Tier1
         {
             _log.Print(LogChecker.Level.Normal, "[method]CastleChunkGenerator.Generate");
 
-            // Initialization step
+            // Initialization
             _availableCells = GetAvailableCells();
             Assert.IsTrue(_availableCells.Count > 0);
+            
             _normProbs = GetNormalizedProbOfEachPoly();
             _normProbsCurrent = (float[]) _normProbs.Clone();
             _pieceTypeSpawnedCounter = new int[_polyominoProvider.Polyominos.Count];
             _placedPolyominos = new List<(Vector2Int pos, Polyomino polyomino)>();
-            var initialEmptyCellCount = _availableCells.Count;
+            _castleCellsCount = _availableCells.Count;
 
             // generation loop
             var fieldCovered = false;
@@ -94,7 +104,7 @@ namespace CastleGenerator.Tier1
                                 int minY = pieceLocationVar.Min(v => v.y);
                                 _placedPolyominos.Add((new Vector2Int(minX, minY), p));
                                 UpdatePerimeter(_perimeter, pieceLocationVar);
-                                _normProbsCurrent = UpdateProbabilities(initialEmptyCellCount);
+                                _normProbsCurrent = UpdateProbabilities(_castleCellsCount);
 
                                 Debug.Log(
                                     $"spawnedCounter:{string.Join('|', _pieceTypeSpawnedCounter)} probs({string.Join(' ', _normProbsCurrent)}); piece index: {pieceIndex}");
@@ -141,7 +151,7 @@ namespace CastleGenerator.Tier1
 
             Debug.Log("done");
         }
-
+        
         public List<(Vector2Int pos, Polyomino polyomino)> GetPlacedPolyominos() => _placedPolyominos;
 
         private List<List<Vector2Int>> ConvertToGlobalPieces(Polyomino omino, Vector2Int pointer)
@@ -219,13 +229,37 @@ namespace CastleGenerator.Tier1
         // expected and real spawn percentages to maintain a balanced distribution.
         private float[] UpdateProbabilities(int initialEmptyCellCount)
         {
-            return _pieceTypeSpawnedCounter
-                .Select((cnt, i) => cnt == 0
-                    ? _normProbs[i]
-                    : _normProbs[i] * (_normProbs[i] * initialEmptyCellCount /
-                                       _polyominoProvider.Polyominos[i].ShapeCells.Count) /
-                      (cnt / (initialEmptyCellCount * 1f / _polyominoProvider.Polyominos[i].ShapeCells.Count)))
-                .ToArray();
+            return _normProbs;
+            // float[] updatedProbabilities = new float[_pieceTypeSpawnedCounter.Length];
+            //
+            // for (int i = 0; i < _pieceTypeSpawnedCounter.Length; i++)
+            // {
+            //     if (_pieceTypeSpawnedCounter[i] == 0)
+            //     {
+            //         updatedProbabilities[i] = _normProbs[i];
+            //     }
+            //     else
+            //     {
+            //         float polyominoCount = _polyominoProvider.Polyominos[i].ShapeCells.Count;
+            //         float factor = _normProbs[i] * (_normProbs[i] * initialEmptyCellCount / polyominoCount) /
+            //                        (_pieceTypeSpawnedCounter[i] / (initialEmptyCellCount * 1f / polyominoCount));
+            //
+            //         updatedProbabilities[i] = _normProbs[i] * factor;
+            //     }
+            // }
+            //
+            // return updatedProbabilities;
+            
+            
+            
+            
+            // return _pieceTypeSpawnedCounter
+            //     .Select((cnt, i) => cnt == 0
+            //         ? _normProbs[i]
+            //         : _normProbs[i] * (_normProbs[i] * initialEmptyCellCount /
+            //                            _polyominoProvider.Polyominos[i].ShapeCells.Count) /
+            //           (cnt / (initialEmptyCellCount * 1f / _polyominoProvider.Polyominos[i].ShapeCells.Count)))
+            //     .ToArray();
         }
 
         private float[] GetNormalizedProbOfEachPoly()
@@ -253,6 +287,44 @@ namespace CastleGenerator.Tier1
         {
             foreach (var placed in _placedPolyominos)
                 Debug.Log($"{placed}");
+        }
+        
+        [Button]
+        private void DbgPrintNormProbs()
+        {
+            int i = 0;
+            foreach (var p in _polyominoProvider.Polyominos)
+            {
+                Debug.Log($" {p.Shape} : {_normProbsCurrent[i]}");
+                ++i;
+            }
+        }
+        
+        [Button]
+        private void DbgPrintPercentage()
+        {
+            int i = 0;
+            foreach (var p in _polyominoProvider.Polyominos)
+            {
+                Debug.Log($" {p.Shape} : {_pieceTypeSpawnedCounter[i]}");
+                ++i;
+            }
+        }
+
+        [Button]
+        private void DbgPrintExpectedPercentage()
+        {
+            var nProb = GetNormalizedProbOfEachPoly();
+            int i = 0;
+            Debug.Log("Estimated polyominos count");
+            foreach (var p in _polyominoProvider.Polyominos)
+            {
+                float cellsAvailable = nProb[i] * (float)_castleCellsCount; // cells available for this  type of polyomino
+                float estimatedPolyominoCount  = Mathf.Round(cellsAvailable / p.ShapeCells.Count);
+                
+                Debug.Log($" {p.Shape} : {estimatedPolyominoCount}");
+                ++i;
+            }
         }
     }
 }
