1adfbd4d7c71804149f25a5c35dbb3fea4d9d2a0
Maxim Chelushkin <maxim.chelushkin@ubisoft.com>
2024-02-14 22:13:23 +0200

    change documentation: castle generator

--- Unity/AGA/Assets/Game/CastleGenerator/T1.Omino/CastleChunkMetaProvider.cs
+++ Unity/AGA/Assets/Game/CastleGenerator/T1.Omino/CastleChunkMetaProvider.cs
@@ -1,9 +1,17 @@
-﻿using TowerGenerator;
+﻿using NaughtyAttributes;
+using TowerGenerator;
+using UnityEngine;
 
 namespace CastleGenerator.Tier1
 {
     public class CastleChunkMetaProvider : MetaProviderGeneric<CastleChunkMeta>
     {
-        
+        [Button()]
+        private void DbgPrintShapes()
+        {
+            int i = 0;
+            foreach (var castleMeta in Metas)
+                Debug.Log($"{i++}. {castleMeta.Shape}");
+        }
     }
 }
