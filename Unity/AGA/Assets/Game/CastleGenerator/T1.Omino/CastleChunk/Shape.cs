d22dd91a3fb7bd759d8764fa7604d64da22e0bff
max <cholushkin@gmail.com>
2024-02-20 02:34:39 +0200

    ***

---
+++ Unity/AGA/Assets/Game/CastleGenerator/T1.Omino/CastleChunk/Shape.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shape : MonoBehaviour
+{
+    public Vector2Int Grid;
+    // public Vector2Int Grid
+    // {
+    //     set => AddGrid(value);
+    // }
+    //
+    // void AddGrid(Vector2Int cell)
+    // {
+    //     Debug.Log(cell);
+    // }
+}
